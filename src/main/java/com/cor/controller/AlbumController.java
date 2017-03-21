17537aa996ea1414b281155a876c29d82f0d4e0e
Galushkin Pavel <gpg@ua.fm>
2017-03-21 22:58:42 +0200

    spring boot

--- src/main/java/com/cor/controller/AlbumController.java
+++ src/main/java/com/cor/controller/AlbumController.java
@@ -1,17 +1,21 @@
 package com.cor.controller;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import com.cor.domain.Album;
+import com.cor.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import com.cor.domain.Album;
-import com.cor.service.MusicService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/rest", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -29,7 +33,6 @@ public class AlbumController {
 			resources.add(getAlbumResource(album));
 		}
 		return resources;
-		
 	}
 
 	@GetMapping(value = "/album/{id}")
@@ -37,12 +40,11 @@ public class AlbumController {
 
 		Album album = musicService.getAlbum(id);
 		return getAlbumResource(album);
-
 	}
 
 	private Resource<Album> getAlbumResource(Album album) {
 
-		Resource<Album> resource = new Resource<Album>(album);
+	    Resource<Album> resource = new Resource<Album>(album);
 
 		// Link to Album
 		resource.add(linkTo(methodOn(AlbumController.class).getAlbum(album.getId())).withSelfRel());
@@ -54,18 +56,14 @@ public class AlbumController {
 		}
 
 		return resource;
-
 	}
 
-	@RequestMapping(value = "/album/purchase/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
+	@GetMapping(value = "/album/purchase/{id}")
 	public Resource<Album> purchaseAlbum(@PathVariable(value = "id") String id) {
-		
 		Album a = musicService.getAlbum(id);
 		a.setStockLevel(a.getStockLevel() - 1);
 		Resource<Album> resource = new Resource<Album>(a);
 		resource.add(linkTo(methodOn(AlbumController.class).getAlbum(id)).withSelfRel());
 		return resource;
-		
 	}
 }
--- src/main/java/com/cor/controller/ArtistController.java
+++ src/main/java/com/cor/controller/ArtistController.java
@@ -1,14 +1,17 @@
 package com.cor.controller;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import com.cor.domain.Artist;
+import com.cor.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import com.cor.domain.Artist;
-import com.cor.service.MusicService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/rest", produces = MediaType.APPLICATION_JSON_VALUE)
